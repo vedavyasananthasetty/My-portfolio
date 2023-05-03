7741881add5ace6cc6710c61b3e41864864cdc7e
Vedavyas Sai Ananthasetty <vedavyasananthasetty@gmail.com>
2023-05-03 06:23:43 -0400

    5th commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,6 +6,8 @@ import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai'
 import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import { useRouter } from 'next/router';
+import vedImg from '/public/assets/ved.png'
+
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -50,7 +52,7 @@ const Navbar = () => {
     <div style={{backgroundColor: `${navBg}`}} className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
         <Link href='/'>
-          <Image src="/../public/assets/ved.png" alt="/" width="125" height="50" />
+          <Image src={vedImg} alt="/" width="125" height="50" />
         </Link>
       <div>
         <ul style={{color: `${linkColor}`}} className='hidden md:flex'>
